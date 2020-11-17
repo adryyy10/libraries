d02b02c919a50d452400f9e6096771fc0d0bcc8d
adryyy10 <adriafigueresgarcia@gmail.com>
2020-11-17 16:18:46 +0100

    Csv bibliotecas + favicon

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,41 @@
 $(document).ready(function() {
     // DATATABLES
-    $('#table_id').DataTable( {
+    $('#table_users').DataTable( {
+        language: {
+            "sProcessing":     "Procesando...",
+            "sLengthMenu":     "Mostrar _MENU_ registros",
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Buscar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "buttons": {
+                "copy": "Copiar",
+                "colvis": "Visibilidad"
+            }
+        }
+    });
+
+    $('#table_bibliotecas').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            { extend: 'csv', text: 'Descargar CSV' }
+        ],
         language: {
             "sProcessing":     "Procesando...",
             "sLengthMenu":     "Mostrar _MENU_ registros",
